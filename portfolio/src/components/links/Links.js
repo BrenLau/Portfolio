84bdc4ce46dfb074332ee12b5b6581d2243c47f7
BrenLau <blau4000@gmail.com>
2023-08-02 16:06:01 -0400

    l

---
+++ portfolio/src/components/links/Links.js
@@ -0,0 +1,15 @@
+const Links = () => {
+    return (
+        <div>
+            <a target="_blank" href='https://www.linkedin.com/in/brendan-l-b6952919a/'>LinkedIn</a>
+            <a>GitHub</a>
+            <a>Project 1</a>
+            <a>Project 2</a>
+            <a>Project 3</a>
+            <a>Project 4</a>
+            <a>Project 5</a>
+        </div>
+    )
+}
+
+export default Links
---
+++ portfolio/src/components/nav/Nav.js
@@ -0,0 +1,13 @@
+const Nav = () => {
+    return (
+        <nav>
+            <div>One</div>
+            <div>Two</div>
+
+            <div>Three</div>
+
+        </nav>
+    )
+}
+
+export default Nav
