23294eab50d2bba5fb5b9d507949a62b9903282d
BrenLau <blau4000@gmail.com>
2023-08-09 23:20:15 -0400

    projects moved

---
+++ portfolio/src/components/projects/Projects.js
@@ -0,0 +1,16 @@
+import "./projects.css"
+
+const Projects = () => {
+    return (
+        <div className='linksdiv'>
+            <a target="_blank" href='https://www.linkedin.com/in/brendan-l-b6952919a/'>LinkedIn</a>
+            <a target="_blank" href='https://github.com/BrenLau'>GitHub</a>
+            <a target="_blank" href='https://the-great-exchange.onrender.com/'>The Great Exchange</a>
+            <a target="_blank" href='https://hiremepls-rsb9.onrender.com/'>HireMePls</a>
+            <a target="_blank" href='https://ninemansocial.herokuapp.com/'>Nineman Social</a>
+            <a target="_blank" href='https://wecord-2.onrender.com/'>Wecord</a>
+        </div>
+    )
+}
+
+export default Projects
