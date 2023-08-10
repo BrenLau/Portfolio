f170f7b9277f67397c9ce55d9b0f5b23aae23612
BrenLau <blau4000@gmail.com>
2023-08-10 15:05:04 -0400

    import

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import Nav from './components/nav/Nav';
 import Links from './components/links/Links';
+import Skills from './components/skills/Skills';
+import Projects from './components/projects/Projects';
 
 function App() {
   return (
     <div className="App">
       <Nav></Nav>
       <Links></Links>
+      <Projects />
+      <Skills />
     </div>
   );
 }
