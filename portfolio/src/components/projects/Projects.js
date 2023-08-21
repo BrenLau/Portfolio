250a4f4be39cd18e18509c9c947902f9c7213b21
BrenLau <blau4000@gmail.com>
2023-08-21 19:02:10 -0400

    added ge images

--- portfolio/src/components/projects/Projects.js
+++ portfolio/src/components/projects/Projects.js
@@ -22,6 +22,21 @@ const Projects = () => {
                     </Carousel.Item>
                 </Carousel>
             </div>
+            <div className="sepprojects">
+                <a className="linktitle" target="_blank" href='https://hiremepls-rsb9.onrender.com/'>HireMePls</a>
+                <Carousel data-bs-theme="dark">
+                    <Carousel.Item>
+                        <img src={hiremeplsfrontpage} className="d-block heightfix" alt='Front Page'></img>
+
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img src={hiremeplsposts} className="d-block heightfix" alt='Front Page'></img>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img src={hiremeplslogin} className="d-block heightfix" alt='Front Page'></img>
+                    </Carousel.Item>
+                </Carousel>
+            </div>
             <div className='projectsdiv'>
                 <a target="_blank" href='https://the-great-exchange.onrender.com/'>The Great Exchange</a>
                 <a target="_blank" href='https://ninemansocial.herokuapp.com/'>Nineman Social</a>
