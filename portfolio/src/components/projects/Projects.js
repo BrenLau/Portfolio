d3ff59c50f73025fe7b46731b5eb7e78fe1c8f42
BrenLau <blau4000@gmail.com>
2023-08-22 00:37:35 -0400

    greatex

--- portfolio/src/components/projects/Projects.js
+++ portfolio/src/components/projects/Projects.js
@@ -3,6 +3,12 @@ import Carousel from 'react-bootstrap/Carousel';
 import hiremeplsfrontpage from './projects/Hiremeplsfrontpage.png'
 import hiremeplsposts from './projects/hiremeplsposts.png'
 import hiremeplslogin from './projects/hiremeplslogin.png'
+import gemain from './projects/GEfrontpage.png'
+import gelivechat from './projects/livechat.png'
+import gemarket from './projects/marketplace.png'
+import geseeking from './projects/seeking.png'
+import geprivatemessage from './projects/privatemessage.png'
+import geprofile from './projects/profile.png'
 
 const Projects = () => {
     return (
@@ -23,17 +29,26 @@ const Projects = () => {
                 </Carousel>
             </div>
             <div className="sepprojects">
-                <a className="linktitle" target="_blank" href='https://hiremepls-rsb9.onrender.com/'>HireMePls</a>
+                <a className="linktitle" target="_blank" href='https://the-great-exchange.onrender.com'>The Great Exchange</a>
                 <Carousel data-bs-theme="dark">
                     <Carousel.Item>
-                        <img src={hiremeplsfrontpage} className="d-block heightfix" alt='Front Page'></img>
+                        <img src={gemain} className="d-block heightfix" alt='Front Page'></img>
 
                     </Carousel.Item>
                     <Carousel.Item>
-                        <img src={hiremeplsposts} className="d-block heightfix" alt='Front Page'></img>
+                        <img src={gelivechat} className="d-block heightfix" alt='Front Page'></img>
                     </Carousel.Item>
                     <Carousel.Item>
-                        <img src={hiremeplslogin} className="d-block heightfix" alt='Front Page'></img>
+                        <img src={gemarket} className="d-block heightfix" alt='Front Page'></img>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img src={geseeking} className="d-block heightfix" alt='Front Page'></img>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img src={geprivatemessage} className="d-block heightfix" alt='Front Page'></img>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img src={geprofile} className="d-block heightfix" alt='Front Page'></img>
                     </Carousel.Item>
                 </Carousel>
             </div>
