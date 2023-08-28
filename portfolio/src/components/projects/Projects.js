3cb84dce4b18eb336b4829a4c6febb946494f8c3
BrenLau <blau4000@gmail.com>
2023-08-28 13:27:35 -0400

    deleted links

--- portfolio/src/components/projects/Projects.js
+++ portfolio/src/components/projects/Projects.js
@@ -100,13 +100,6 @@ const Projects = () => {
                 }
             </div>
 
-
-
-            <div className='projectsdiv'>
-                <a target="_blank" href='https://the-great-exchange.onrender.com/'>The Great Exchange</a>
-                <a target="_blank" href='https://ninemansocial.herokuapp.com/'>Nineman Social</a>
-                <a target="_blank" href='https://wecord-2.onrender.com/'>Wecord</a>
-            </div>
         </>
     )
 }
