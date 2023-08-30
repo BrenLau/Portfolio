5989d7731d7f4ea71333168612ae9ea35369971b
BrenLau <blau4000@gmail.com>
2023-08-30 02:16:55 -0400

    summary

---
+++ portfolio/src/components/summary/Summary.js
@@ -0,0 +1,8 @@
+const Summary = () => {
+    return (
+        <div>
+            I am a software engineer who graduated from App Academy's full stack web development boot camp. App Academy is a vigorous six-month program designed to jam years of software knowledge and work applicable skills in a short period of time. This helped me learn how to grow at a rapid rate, as I was able to catch on to concepts and technologies relatively quickly. I was able to pass every coding exam with a perfect score, demonstrating my ability to problem solve with newfound knowledge.
+        </div>
+    )
+}
+export default Summary
